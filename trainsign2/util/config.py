9910c531892771bbd0c05fca7b4539024dd66519
James Taliaferro <me@jamestali.com>
2023-08-12 12:03:57 -0700

    add basic schema and logic to load the config file

--- trainsign2/util/config.py
+++ trainsign2/util/config.py
@@ -1,5 +1,14 @@
 import os
+from typing import Sequence
+
+from yaml import safe_load
+from pydantic import BaseModel, HttpUrl
+
 
 def load_config(path: os.PathLike):
     with open(path, "r") as config_fh:
-        return config_fh.read()
+        return Config(**safe_load(config_fh))
+
+class Config(BaseModel):
+    init: Sequence[str]
+    loop: Sequence[str]
