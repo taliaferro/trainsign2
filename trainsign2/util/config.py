895600a2880967fbc38e0ee3e182faa4cc8bc672
James Taliaferro <me@jamestali.com>
2023-08-12 12:52:32 -0700

    configure and initialize serial port

--- trainsign2/util/config.py
+++ trainsign2/util/config.py
@@ -2,7 +2,7 @@ import os
 from typing import Sequence
 
 from yaml import safe_load
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, FilePath
 
 
 def load_config(path: os.PathLike):
@@ -10,6 +10,14 @@ def load_config(path: os.PathLike):
         return Config(**safe_load(config_fh))
 
 
+class SerialConfig(BaseModel):
+    # could need the rest of the parameters in other use cases,
+    # but these two are enough to get it working with an Arduino
+    port: str
+    baudrate: int = 9600
+
+
 class Config(BaseModel):
+    serial: SerialConfig
     init: Sequence[str]
     loop: Sequence[str]
