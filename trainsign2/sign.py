a2d8cd4022c3db3888c8ce1fa1d52770e660de68
James Taliaferro <taliaferro1@llnl.gov>
2023-12-23 17:16:19 -0700

    configure logger based on command line

--- trainsign2/sign.py
+++ trainsign2/sign.py
@@ -1,4 +1,6 @@
 import os
+import logging
+import logging.config
 import asyncio
 from typing import Optional, Iterable
 
@@ -6,7 +8,7 @@ from serial import Serial
 from jinja2 import Environment, Template
 
 from trainsign2.client import Open511Client
-from trainsign2.util.config import load_config, Screen
+from trainsign2.util.config import load_config, Config, Screen
 from trainsign2.util.helpers import time_format
 
 
@@ -18,18 +20,25 @@ def _prepare_msg(message: str):
 
 
 class Sign:
+
+    config: Config
+
     def __init__(self, config: Optional[os.PathLike]):
         self.config = load_config(config)
-        self._serial = Serial(**self.config.serial.model_dump())
 
+        logging.config.dictConfig(self.config.logging.model_dump(by_alias=True))
+
+        self._logger = logging.getLogger(__name__)
+
+        self._serial = Serial(**self.config.serial.model_dump())
         self._template_environment = Environment()
         self._template_environment.globals["now"] = time_format
 
         self._client = Open511Client(**self.config.client.model_dump())
 
         self.init_screens = [self._load_screen(screen) for screen in self.config.init]
-
         self.loop_screens = [self._load_screen(screen) for screen in self.config.loop]
+
         self.arrivals = []
 
     async def start(self):
@@ -39,8 +48,9 @@ class Sign:
         ]
         forever_tasks = [self._update_loop(), self._display_loop()]
 
+        self._logger.info("Running initialization tasks")
         await asyncio.gather(*init_tasks)
-        print("beginning forever tasks")
+        self._logger.info("Running repeated tasks")
         await asyncio.gather(*forever_tasks)
 
     async def _display_loop(self):
@@ -53,6 +63,7 @@ class Sign:
 
     async def _update_arrivals_cache(self):
         latest_monitoring = self._client.stop_monitoring()
+        self._logger.info("Updating arrivals cache...")
         self.arrivals = latest_monitoring["ServiceDelivery"]["StopMonitoringDelivery"][
             "MonitoredStopVisit"
         ]
@@ -66,6 +77,7 @@ class Sign:
             await self._show_screen(screen, template)
 
     async def _show_screen(self, screen: Screen, template: Template):
+        self._logger.info("Displaying screen {}".format(screen.template))
         self.write(template.render(context=screen.context))
         await asyncio.sleep(screen.duration)
 
